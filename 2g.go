8b2fafce4e29eaa7b66de1f8b79f2066c061a026
daimatz <dai@daimatz.net>
2014-06-30 02:29:57 +0900

    read dat files

--- 2g.go
+++ 2g.go
@@ -3,14 +3,44 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+
+	"code.google.com/p/go.text/encoding/japanese"
+	"code.google.com/p/go.text/transform"
 )
 
-func fetch(dat chan string, done chan bool) {
+func Dat(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("failed to fetch: %v, err: %v\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	reader := transform.NewReader(resp.Body, japanese.ShiftJIS.NewDecoder())
+	bufr := bufio.NewReader(reader)
 	for {
-		d, more := <-dat
-		if more && d != "" {
-			fmt.Println(d)
+		lineb, err := bufr.ReadString('\n')
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatalf("failed to read content. err: %v\n", err)
+			return
+		}
+		line := string(lineb)
+		fmt.Printf("line: %v\n", string(line))
+	}
+}
+
+func DatQueue(dat chan string, done chan bool) {
+	for {
+		url, more := <-dat
+		if more && url != "" {
+			Dat(url)
 		} else {
 			done <- true
 		}
@@ -21,7 +51,7 @@ func main() {
 	dat := make(chan string)
 	done := make(chan bool)
 
-	go fetch(dat, done)
+	go DatQueue(dat, done)
 
 	if len(os.Args) > 1 {
 		for i := 1; i < len(os.Args); i++ {
